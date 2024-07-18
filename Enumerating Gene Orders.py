afa3d148c1700e65d38ba61ab9fbd29288f75f0e
Gul Bulbul <79763247+gbulbul@users.noreply.github.com>
2024-07-18 12:35:17 -0400

    Update Enumerating Gene Orders.py

--- Enumerating Gene Orders.py
+++ Enumerating Gene Orders.py
@@ -1,16 +1,23 @@
-import itertools
-n=3
-list_possible= list(range(1,n+1))
-def possible(list_possible):
-    list_of_tuples=[]
-    list_of_tuples.extend(itertools.permutations(list_possible))
-    return list_of_tuples
-tuple_list=possible(list_possible)
-tuple_list.insert(0,(len(tuple_list), '', ''))
-import pandas as pd
-df = pd.DataFrame(tuple_list, columns=['', '', ''])
-print(df.to_string(index=False))
-            
+class obtain_triples:
+    def alternative_triples(n):
+        list_of_tuples=[]
+        for i in range(1,n+1,1):
+            for j in range(1,n+1,1):
+                for k in range(1,n+1,1):
+                    if i!=k and i!=j and k!=j:
+                       tuple_=(i,j,k)
+                       list_of_tuples.append(tuple_)
+        return list_of_tuples
+    
+    def arrange_output(list_of_tuples):
+        list_of_tuples.insert(0,(len(list_of_tuples), '', ''))
+        df = pd.DataFrame(list_of_tuples, columns=['', '', ''])
+        return print(df.to_string(index=False))
+if __name__=="__main__":
+    import pandas as pd
+    n=3
+    list_of_tuples=obtain_triples.alternative_triples(n)
+    obtain_triples.arrange_output(list_of_tuples)
         
 
 
